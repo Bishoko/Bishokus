4e3caad44b2a9380a97f7bb909fbaed3da1c31ec
Lenoch <contact.lenoch@gmail.com>
2026-04-25 00:00:02 +0200

    Fix ratio defer error

--- commands/fun/ratio.py
+++ commands/fun/ratio.py
@@ -48,6 +48,8 @@ async def ratio_context(lang: str, interaction: nextcord.Interaction, original_m
     #     inline=False
     # )
     
+    await interaction.response.defer(ephemeral=True)
+    
     if original_message.attachments:
         view = nextcord.ui.View()
         view.add_item(nextcord.ui.Button(
@@ -58,23 +60,13 @@ async def ratio_context(lang: str, interaction: nextcord.Interaction, original_m
     else:
         sent_message = await original_message.reply(embed=embed, mention_author=False)
     
-    if not interaction.guild_id:
-        # If this is a DM, sending the response before the reactions works better,
-        # otherwise, it can cause an error 50% of the time
-        # FIXME: this still causes an error sometimes, maybe it's a rate limit issue?
-        await interaction.response.send_message(
-            text('ratio_context_success', lang),
-            ephemeral=True
-        )
     
     await sent_message.add_reaction(up_emoji)
     await sent_message.add_reaction(down_emoji)
     
-    if interaction.guild_id:
-        await interaction.response.send_message(
-            text('ratio_context_success', lang),
-            ephemeral=True
-        )
+    await interaction.edit_original_message(
+        content=text('ratio_context_success', lang)
+    )
 
 
 info = {
